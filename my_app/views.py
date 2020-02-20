eaf4c30040171920699687e75cbc811d495e2fe2
Aleksander Gorecki <alegorecki@gmail.com>
2020-02-20 23:23:14 +0100

    end of frontend 5:32:35

--- my_app/views.py
+++ my_app/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+import request
+from bs4 import BeautifulSoup
 
-# Create your views here.
+BASE_CRAIGSLIST_URL = "https://sfbay.craigslist.org/search/?query={}"
 def home(request):
     return render(request, 'base.html')
 
 def new_search(request):
     search = request.POST.get('search')
+    data = request.get()
     print(search)
     context = {
         'search': search,
